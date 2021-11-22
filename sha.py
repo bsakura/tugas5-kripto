cc86c661856eef03769d348090090ab4debf2a59
bsakura <13518066@std.stei.itb.ac.id>
2021-11-22 22:59:10 +0700

    Update sha.py

--- sha.py
+++ sha.py
@@ -1,5 +1,10 @@
+def chunker(bits, chunk_length=8):
+    chunked = []
+    for b in range(0, len(bits), chunk_length):
+        chunked.append(bits[b:b+chunk_length])
+    return chunked
+
 def translate(message):
-    # print(message)
     charcodes = [ord(c) for c in message]
     bytes = []
     for char in charcodes:
@@ -10,13 +15,7 @@ def translate(message):
             bits.append(int(bit))
     return bits
 
-def chunker(bits, chunk_length=8):
-    chunked = []
-    for b in range(0, len(bits), chunk_length):
-        chunked.append(bits[b:b+chunk_length])
-    return chunked
-
-def fillZeros(bits, length=8, endian='LE'):
+def addZeros(bits, length=8, endian='LE'):
     l = len(bits)
     if endian == 'LE':
         for i in range(l, length):
@@ -33,12 +32,12 @@ def preprocessMessage(message):
     message_len = [int(b) for b in bin(length)[2:].zfill(64)]
     if length < 448:
         bits.append(1)
-        bits = fillZeros(bits, 448, 'LE')
+        bits = addZeros(bits, 448, 'LE')
         bits = bits + message_len
         return [bits]
     elif length == 448:
         bits.append(1)
-        bits = fillZeros(bits, 1024, 'LE')
+        bits = addZeros(bits, 1024, 'LE')
         bits[-64:] = message_len
         return chunker(bits, 512)
     else:
@@ -47,17 +46,6 @@ def preprocessMessage(message):
             bits.append(0)
         bits[-64:] = message_len
     return chunker(bits, 512)
-
-
-def initializer(values):
-    binaries = [bin(int(v, 16))[2:] for v in values]
-    words = []
-    for binary in binaries:
-        word = []
-        for b in binary:
-            word.append(int(b))
-        words.append(fillZeros(word, 32, 'BE'))
-    return words
 
 def b2Tob16(value):
     value = ''.join([str(x) for x in value])
@@ -69,27 +57,52 @@ def b2Tob16(value):
         hexes += hex(int(b ,2))[2:]
     return hexes
 
-def isTrue(x): return x == 1
+def initializer(values):
+    binaries = [bin(int(v, 16))[2:] for v in values]
+    words = []
+    for binary in binaries:
+        word = []
+        for b in binary:
+            word.append(int(b))
+        words.append(addZeros(word, 32, 'BE'))
+    return words
 
-def if_(i, y, z): return y if isTrue(i) else z
 
-def and_(i, j): return if_(i, j, 0)
-def AND(i, j): return [and_(ia, ja) for ia, ja in zip(i,j)] 
 
-def not_(i): return if_(i, 0, 1)
-def NOT(i): return [not_(x) for x in i]
+def isTrue(x): 
+    return x == 1
 
-def xor(i, j): return if_(i, not_(j), j)
-def XOR(i, j): return [xor(ia, ja) for ia, ja in zip(i, j)]
+def if_(i, y, z): 
+    return y if isTrue(i) else z
 
-def xorxor(i, j, l): return xor(i, xor(j, l))
-def XORXOR(i, j, l): return [xorxor(ia, ja, la) for ia, ja, la, in zip(i, j, l)]
+def and_(i, j): 
+    return if_(i, j, 0)
+def AND(i, j): 
+    return [and_(ia, ja) for ia, ja in zip(i,j)] 
 
-def maj(i,j,k): return max([i,j,], key=[i,j,k].count)
+def not_(i): 
+    return if_(i, 0, 1)
+def NOT(i): 
+    return [not_(x) for x in i]
 
-def rotr(x, n): return x[-n:] + x[:-n]
+def xor(i, j): 
+    return if_(i, not_(j), j)
+def XOR(i, j): 
+    return [xor(ia, ja) for ia, ja in zip(i, j)]
 
-def shr(x, n): return n * [0] + x[:-n]
+def xorxor(i, j, l): 
+    return xor(i, xor(j, l))
+def XORXOR(i, j, l): 
+    return [xorxor(ia, ja, la) for ia, ja, la, in zip(i, j, l)]
+
+def maj(i,j,k): 
+    return max([i,j,], key=[i,j,k].count)
+
+def rotr(x, n): 
+    return x[-n:] + x[:-n]
+
+def shr(x, n): 
+    return n * [0] + x[:-n]
 
 def add(i, j):
     length = len(i)
@@ -162,10 +175,3 @@ def sha256(message):
         digest += b2Tob16(val)
     return digest
 
-if __name__ == '__main__':
-    verdict = 'y'
-    while verdict == 'y':
-        input_message = input('Type or copy your message here: ')
-        print('Your message: ', input_message)
-        print('Hash: ', sha256(input_message))
-        verdict = input('Do you want to try another text? (y/n): ').lower()
